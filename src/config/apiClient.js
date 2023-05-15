e7b74675e5bf1be4280c7690f66c2009f124ead1
Aeon <88754929+FabioVLucena@users.noreply.github.com>
2023-05-14 21:31:20 -0400

    Update apiClient.js

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://json-server-sigma-lilac.vercel.app/",
   headers: {
     "Content-type": "application/json"
   }
